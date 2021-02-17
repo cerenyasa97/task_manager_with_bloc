8ac68cfe7b610c8156bbd4ed266b09ca0f70b546
Ceren Yasa <cerenyasa97@hotmail.com>
2021-02-18 01:23:28 +0300

    The constants and widgets used in many files in the program were divided into different files.

---
+++ lib/widgets/error_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaskErrorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Error occured! Please refresh..."),
+    );
+  }
+}
